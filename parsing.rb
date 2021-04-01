64cbcd64f7d71be788962b49b6f92f5cb3792492
Begana Choi <begana001.jpg@gmail.com>
2021-04-01 14:45:55 +0200

    feat(parsing): add nokogiri

---
+++ parsing.rb
@@ -0,0 +1,23 @@
+require 'nokogiri'
+require 'open-url'
+require 'recipe'
+
+class Parsing
+  def initialize(ingredient)
+    @ingredient = ingredient
+  end
+
+  def call
+    url = "https://www.allrecipes.com/search/results/?search=#{@ingredient}"
+    html_doc = Nokogiri::HTML(open(url), nil, 'utf-8')
+
+    result = []
+
+    html_doc.search('.card-recipe').first(5).each do |element|
+      name = element.search('.card__title').text.strip
+      description = element.search('.card__summary').text.strip
+      result << Recipe.new(name, description)
+    end
+    result
+  end
+end
